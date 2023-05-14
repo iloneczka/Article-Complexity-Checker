7215ae7800ddabb886cc882c1a905929613f4b4f
Ilona Pokrywka <ilo@MacBook-Air-Ilona.local>
2023-05-14 14:32:14 +0200

    Refactoring codu do postaci funkcyjnej.

--- Projekt 1.py
+++ Projekt 1.py
@@ -1,10 +1,30 @@
-### 🔴 Projekt
+### 🔴 Ćwiczenie
 
-# Napisz dla BBC program sprawdzający złożoność artykułów i wpisów, dzięki czemu pracę dziennikarzy będzie można sparametryzować i automatycznie ustalić, czy piszą teksty proste i łatwe w zrozumieniu. Policz jaka jest średnia długość słów i wyświetl rezultat.
+# Popraw program M01L13. W tym programie sprawdzaliśmy złożoność tekstów licząc jaka jest średnia długość słów. Na tamtym etapie nie znaliśmy metody jak wyliczyć tą wielkość dokładnie, bez spacji, znaków interpunkcyjnych czy też pomijając liczby w tekście.
 
-import re
-text= input("Wpisz tekst: ")
-number_letters= len(re.findall("[a-zA-Z0-9]",text))  #ilość wszystkich liter bez spacji i znaków specjalnych.
-number_words= len(text.split())  #ilosc slów
-average = number_letters / number_words
-print("Słowa w Twoim tekście mają średnio", average, "znaków")
+# Popraw kod tak, aby nie zliczał spacji ani znaków interpunkcyjnych. Dodatkowo, jeśli w tekście pojawiają się liczby, to również nie bierz ich pod uwagę.
+
+import string
+
+PUNCTUATION= string.punctuation
+
+def remove_punctuation(text):
+    for char in PUNCTUATION:
+        text= text.replace(char, "")
+    return text
+
+def calculate_average_word_length(text):
+    words = remove_punctuation(text).split()
+    without_numbers= [w for w in words if not w.isnumeric()]
+    length= [len(w) for w in without_numbers]
+    average= sum(length) / len(length)
+    return average
+
+def main():
+    user_text= input("Write your text: ")
+
+    average_lenght= calculate_average_word_length(user_text)
+    print("Średnia długość słów podanego tekstu wynosi:", average_lenght)
+
+if __name__ == "__main__":
+    main()
